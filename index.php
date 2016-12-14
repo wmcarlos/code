201e33f93cc0f79116506f72b80b747e7f55ac9b
Carlos Vargas <info-analista7@iancarina.com.ve>
2016-12-14 15:12:56 -0400

    change the structure for directory

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	require_once("models/model_helper.php");
-	$helper = new model_helper();
-	include("header.php");
-	include("body.php");
-	include("footer.php");
-?>
--- views/view_access.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-	<div class="panel panel-default">
-		<div class="panel-heading"><h4>Access</h4></div>
-		<div class="panel-body">
-			
-		</div>
-	</div>
-</div>
