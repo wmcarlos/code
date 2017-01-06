235f00b3723f80562bf4b1225386492c4d9230e7
Carlos Vargas <info-analista7@iancarina.com.ve>
2017-01-06 10:53:01 -0400

    updating the folder project structure

---
+++ content/templates/gissel/tinfo.php
@@ -0,0 +1,9 @@
+<?php 
+	$template = Array(
+		"name" => "Gissel",
+		"author" => "Carlos Vargas",
+		"version" => "1.0",
+		"email" => "cVargas@frontuari.com",
+		"website" => "http://www.frontuari.com"
+	);
+?>
